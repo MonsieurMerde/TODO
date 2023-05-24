5a57e7fc820cc52699626917605e823ee6739ac5
Joe <totenlciht@mail.ru>
2023-05-24 17:37:31 +0500

    lesson_01: Dump users data into fixture

--- users/models.py
+++ users/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 
 class User(AbstractUser):
+    def save(self, *args, **kwargs):
+        self.password = make_password(self.password)
+        super().save(*args, **kwargs)
     email = models.EmailField(_('email address'), blank=True, unique=True)
