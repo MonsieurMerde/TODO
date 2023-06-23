be83dce90b6b94ed529a85029af360a4e373c237
Joe <totenlciht@mail.ru>
2023-06-23 21:44:05 +0500

    lesson_02: componentDidMount added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,21 +4,55 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
+    constructor(props) {
+        super(props)
 
-    this.state = {
-      'users': []
+        this.state = {
+        'users': []
+        }
     }
-  }
 
-  render() {
-    return (
-      <div>
-        <UserList users={this.state.users} />
-      </div>
-    )
-  }
-}
+    componentDidMount() {
+        const users = [
+        {
+            "username": "admin",
+            "first_name": "",
+            "last_name": "",
+            "email": "admin@todo.com"
+        },
+        {
+            "username": "user1",
+            "first_name": "",
+            "last_name": "",
+            "email": "user1@todo.com"
+        },
+        {
+            "username": "user2",
+            "first_name": "",
+            "last_name": "",
+            "email": "user2@todo.com"
+        },
+        {
+            "username": "moderator1",
+            "first_name": "",
+            "last_name": "",
+            "email": "moderator1@todo.com"
+        }
+    ]
+    this.setState(
+        {
+            'users': users
+        }
+    )  
+    }
+
+    render() {
+        return (
+        <div>
+            <UserList users={this.state.users} />
+        </div>
+        )
+    }
+    }
 
 export default App;
