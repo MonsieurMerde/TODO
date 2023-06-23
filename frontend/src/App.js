88ad99bf272d014dc4fe31c889b8c6ac1d45946b
Joe <totenlciht@mail.ru>
2023-06-23 22:22:35 +0500

    lesson_02: axios added in componentDidMount

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import UserList from './components/UserList';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
@@ -13,37 +13,17 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        const users = [
-        {
-            "username": "admin",
-            "first_name": "",
-            "last_name": "",
-            "email": "admin@todo.com"
-        },
-        {
-            "username": "user1",
-            "first_name": "",
-            "last_name": "",
-            "email": "user1@todo.com"
-        },
-        {
-            "username": "user2",
-            "first_name": "",
-            "last_name": "",
-            "email": "user2@todo.com"
-        },
-        {
-            "username": "moderator1",
-            "first_name": "",
-            "last_name": "",
-            "email": "moderator1@todo.com"
-        }
-    ]
-    this.setState(
-        {
-            'users': users
-        }
-    )  
+        axios.get('http://127.0.0.1:8000/api/users/')
+            .then(response => {
+                const users = response.data
+                this.setState(
+                    {
+                        'users': users
+                    }
+                )  
+            }
+            )
+            .catch(error => console.log(error))
     }
 
     render() {
