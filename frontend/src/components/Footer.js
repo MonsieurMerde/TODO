532baff8c21651785e756f8c6773b8b973834247
Evgeniy Chernykh <90401654+MonsieurMerde@users.noreply.github.com>
2023-08-07 20:19:50 +0500

    Lesson 02 (#2)

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+    return (
+      <footer>
+        <div>
+          TODO
+        </div>
+      </footer>
+    );
+  };
+
+export default Footer;
