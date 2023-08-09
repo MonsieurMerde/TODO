232503442adaa9f78eaa7080288e6a0d453d592e
Joe <totenlciht@mail.ru>
2023-08-09 17:32:54 +0500

    lesson_03: API to Project and ToDo models

---
+++ todo/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework.serializers import HyperlinkedModelSerializer, StringRelatedField
+
+from .models import Project, ToDo
+
+
+class ProjectModelSerializer(HyperlinkedModelSerializer):
+    project_team = StringRelatedField(many=True)
+
+    class Meta:
+        model = Project
+        fields = [
+            "project_name",
+            "link",
+            "description",
+            "project_team",
+        ]
+
+
+class ToDoModelSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = ToDo
+        fields = [
+            "project",
+            "text",
+            "user_created",
+            "is_active",
+        ]
