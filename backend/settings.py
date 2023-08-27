55610a699a92beb856be67bece902b0a35aa51b5
Evgeniy Chernykh <90401654+MonsieurMerde@users.noreply.github.com>
2023-08-27 19:40:52 +0500

    Lesson 04 (#4)

--- backend/settings.py
+++ backend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "corsheaders",
     "users",
     "todo",
+    "django_filters",
 ]
 
 MIDDLEWARE = [
@@ -141,5 +142,8 @@ REST_FRAMEWORK = {
         "djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer",
         # "rest_framework.renderers.JSONRenderer",
         # "rest_framework.renderers.BrowsableAPIRenderer",
-    ]
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 2,
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
 }
---
+++ todo/filters.py
@@ -0,0 +1,20 @@
+from django_filters.rest_framework import CharFilter, FilterSet
+
+from todo.models import Project, ToDo
+
+
+class ProjectFilter(FilterSet):
+    project_name = CharFilter(lookup_expr="contains")
+
+    class Meta:
+        model = Project
+        fields = ["project_name"]
+
+
+class ToDoFilter(FilterSet):
+    class Meta:
+        model = ToDo
+        fields = {
+            "project": ["exact"],
+            "created": ["gt", "lt"],
+        }
