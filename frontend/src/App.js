4920fa2eafb58c15b6d622ee89783b8556b9233c
Joe <totenlciht@mail.ru>
2023-10-13 14:22:37 +0500

    lesson_07: authorization component with login and password forms added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import ProjectList from './components/ProjectList';
 import ProjectToDoList from './components/ProjectToDoList';
 import ToDoList from './components/ToDoList';
 import UserList from './components/UserList';
+import LoginForm from './components/Auth';
 import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
 import axios from 'axios';
 import './App.css';
@@ -70,6 +71,7 @@ class App extends React.Component {
                         <Route path=':projectID' element={<ProjectToDoList todos={this.state.todos} />} />
                     </Route>
                     <Route exact path='/todo' element={<ToDoList todos={this.state.todos} />} />
+                    <Route exact path='/login' element={<LoginForm />} />
                     <Route exact path='/' element={<Navigate to='/users' />} />
                     <Route path='*' element={<NotFound404 />} />
                 </Routes>
