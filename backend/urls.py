edc3a77de1b0db42dc8be53d5995da14c3e0d0a5
Joe <totenlciht@mail.ru>
2023-11-09 12:39:26 +0500

    lesson_09: Documentation to API with using drf-yasg added

--- backend/urls.py
+++ backend/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from drf_yasg.openapi import Contact, Info, License
+from drf_yasg.views import get_schema_view
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
@@ -27,12 +29,30 @@ router.register("users", UserModelViewSet)
 router.register("projects", ProjectModelViewSet)
 router.register("todo", ToDoModelViewSet)
 
+schema_view = get_schema_view(
+    Info(
+        title="ToDo Task List",
+        default_version="1.0",
+        description="Documentation to ToDo API",
+        contact=Contact(email="boss@todo.com"),
+        license=License("MIT License"),
+    ),
+    public=True,
+)
+
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/", include(router.urls)),
+    path("admin/", admin.site.urls, name="admin"),
+    path("api/", include(router.urls), name="api"),
     path("api-auth/", include("rest_framework.urls")),
-    path("api-token-auth/", views.obtain_auth_token),
+    path(
+        "api-token-auth/",
+        views.obtain_auth_token,
+        name="token_auth",
+    ),
     path("api-jwt/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api-jwt/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api-jwt/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("swagger/", schema_view.with_ui(), name="schema_swagger"),
+    path("redoc/", schema_view.with_ui("redoc"), name="schema_redoc"),
+    re_path(r"swagger(?P<format>\.json|\.yaml)", schema_view.without_ui(), name="schema_json"),
 ]
