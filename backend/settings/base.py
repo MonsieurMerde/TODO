dbfe5551c363e74058870640ed1a518af746ba13
Joe <totenlciht@mail.ru>
2023-12-21 17:38:54 +0500

    lesson_12: base, debug, debug with postgres, production settings added

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-4!+sq#wd2q)7$*8jo9=op_vbptll2e_5v#mkd702nfe1nb%cg0"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
---
+++ backend/settings/production.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "todo",
+        "USER": "admin",
+        "PASSWORD": "admin",
+        "HOST": "db",
+        "PORT": "5432",
+    }
+}
